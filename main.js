168529946f707bc62b1e533ca7194ac69ee82893
Tamara Gacsadi <16083469+tamaragacsadi@users.noreply.github.com>
2023-05-17 21:49:50 +0200

    Handle click outside of menu

--- main.js
+++ main.js
@@ -10,15 +10,25 @@
         return el
     }
 
-    //Show or hide menu
+    // Show or hide menu
     const toggleHamburger = () => {
       $('.nav-burger__container').classList.toggle('nav-burger__container--toggle')
     }
 
+    // Handle click outside of menu
+    const clickOutsideBurgerMenu = e => {
+      const navButton = document.getElementById('nav-button__container')
+      const navMenu = document.getElementById('nav-burger__container')
+
+      if (navButton.contains(e.target) || 
+      (!navMenu.contains(e.target) && navMenu.classList.contains('nav-burger__container--toggle')))
+        toggleHamburger()
+    }
+
     const init = () => {
-      $on(
-          $on($('.nav-button'), 'click', () => (toggleHamburger())),
-      )
-  }
+      $on($('.nav-burger__container'), 'click', () => (toggleHamburger()))
+      $on(window, 'click', e => (clickOutsideBurgerMenu(e)))
+    }
+
   init()
 }
